d7216d2a468647e667b99d269341d08de3fc7321
Dawid Trojan <daw.trojan@gmail.com>
2019-08-12 11:46:41 +0200

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import "../sass/main.scss";
 
-
-function App() {
-  
-}
+function App() {}
 
 export default App;
