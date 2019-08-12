757f2499287b081b628bee6b0dc236c1af11bdfc
Dawid Trojan <daw.trojan@gmail.com>
2019-08-12 14:57:02 +0200

    first changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import "../sass/main.scss";
+import Header from "./Header";
+import Footer from "./Footer";
 
-function App() {}
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch />
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
