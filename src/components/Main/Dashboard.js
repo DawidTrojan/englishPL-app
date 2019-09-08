eba164aadec9f8e341f6d98d8a3ba66e18a5a3a1
Dawtro <51854427+Dawtro@users.noreply.github.com>
2019-09-08 11:34:55 +0200

    main section styles

--- src/components/Main/Dashboard.js
+++ src/components/Main/Dashboard.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
 
 const Dashboard = () => (
-  <Row className="dashboard_box">
-    <Col className="dashboard_content">
+  <div className="dashboard_box">
+    <div className="dashboard_title">
       <h2>Welcome to English Premier League!</h2>
-      <h4>
+    </div>
+    <div className="dashboard_content">
+      <span>
         You can check the standings, results, scorers and players. The content
         is updated regularly after each match.
-      </h4>
+      </span>
       <NavLink className="dashboard_link" to="/signup">
         Register and choose your favorite team!
       </NavLink>
-    </Col>
-  </Row>
+    </div>
+  </div>
 );
 
 export default Dashboard;
