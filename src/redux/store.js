b05b11639be58120d58f0fc0c9b669375ac904ed
Dawtro <51854427+Dawtro@users.noreply.github.com>
2019-12-12 14:29:39 +0100

    modified store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import thunk from "redux-thunk";
 import reducer from "./reducers";
 import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import firebase from "../config/firebase";
+import firebase from "../firebase/firebase";
 
 const store = createStore(
   reducer,
